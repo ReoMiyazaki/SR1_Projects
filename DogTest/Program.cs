82ba054368b6df57e7b1b147cb8f24d298d0d5a8
REO MIYA <41591rionmiyazaki@gmail.com>
2023-10-22 12:50:49 +0900

    プロジェクト ファイルを追加します。

---
+++ DogTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dogクラスのインスタンス hachi を生成する
+            Dog hachi = new Dog();
+            hachi.Brak();
+
+            Dog pochi = new Dog();
+            pochi.Brak();
+            pochi.Run();
+
+            // 一時停止
+            Console.ReadLine();
+        }
+    }
+}
